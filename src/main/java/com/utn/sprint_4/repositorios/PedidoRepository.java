a45987bfe478c9ea57ac895407506dda76bbe9fb
vickyvelas <80783514+vickyvelas@users.noreply.github.com>
2023-11-15 16:15:08 -0300

    Update PedidoRepository.java

--- src/main/java/com/utn/sprint_4/repositorios/PedidoRepository.java
+++ src/main/java/com/utn/sprint_4/repositorios/PedidoRepository.java
@@ -58,10 +58,10 @@ public interface PedidoRepository extends BaseRepository<Pedido,Long> {
             nativeQuery = true)
     List<Pedido> searchNativoPedidosEstado(@Param("estado") EstadoPedido estado, Pageable pageable);
 
-    /@Query(value = "SELECT * FROM pedido WHERE pedi")/
+    //@Query(value = "SELECT * FROM pedido WHERE pedi")
 
-    @Query("SELECT p FROM Pedido p WHERE p.fecha BETWEEN :fechaInicio AND :fechaFin")
-    List<Pedido> pedidosEntreFechas(@Param("fechaInicio") java.util.Date fechaInicio, @Param("fechaFin") java.util.Date fechaFin);
+    //@Query("SELECT p FROM Pedido p WHERE p.fecha BETWEEN :fechaInicio AND :fechaFin")
+    //List<Pedido> pedidosEntreFechas(@Param("fechaInicio") java.util.Date fechaInicio, @Param("fechaFin") java.util.Date fechaFin);
 
 
 }
